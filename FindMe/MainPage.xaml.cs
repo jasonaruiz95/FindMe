17242a1fc97ada5f96d3ee6d67721130e85978d5
Jason Ruiz <jasonruiz1@mail.weber.edu>
2024-11-29 18:51:42 -0700

    initial commit

--- FindMe/MainPage.xaml.cs
+++ FindMe/MainPage.xaml.cs
@@ -2,24 +2,62 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
-	public MainPage()
+    string _baseUrl = "https:/ /bing.com/maps/default.aspx?cp=";
+    public string UserName { get; set; }
+
+    public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	
+	private async Task ShareLocation()
 	{
-		count++;
+		UserName = UsernameEntry.Text;
+        var locationRequest = new GeolocationRequest(GeolocationAccuracy.Best);
+		var location = await Geolocation.GetLocationAsync(locationRequest);
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+		await Share.RequestAsync(new ShareTextRequest
+		{
+			Subject = "Find me!",
+			Title = "Find me!",
+			Text = $"{UserName} is sharing their location with you",
+			Uri = $"{_baseUrl}{location.Latitude}~{location.Longitude}"
+		});
+    }
+
+	private async void OnFindMeClicked(object sender, EventArgs e)
+	{
+		var permissions = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+		if (permissions == PermissionStatus.Granted)
+		{
+			await ShareLocation();
+		}
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+		{
+			await App.Current.MainPage.DisplayAlert("Permissions Error", "You have not granted the apppermission to access your location.", "OK");
+			var requested = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+			if (requested == PermissionStatus.Granted)
+			{
+				await ShareLocation();
+			}
+			else
+			{
+				if (DeviceInfo.Platform == DevicePlatform.iOS || DeviceInfo.Platform == DevicePlatform.MacCatalyst)
+				{
+					await App.Current.MainPage.DisplayAlert("Location Required", "Location is required to share it. Please enable location for this app in Settings.", "OK");
+				}
+				else
+				{
+					await App.Current.MainPage.DisplayAlert("Location Required", "Location is required to share it. We'll ask again next time.", "OK");
+				}
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+			}
+        }
+
+    }
 }
 
 
